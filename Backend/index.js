69abef6b116fc01a0ef134574d2b26a66f72ac8f
PAVAN VANAM <pavan.vanam@aceec.ac.in>
2025-05-20 11:40:28 +0530

    redeploy

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-export async function init() {
+async function init() {
   try {
     await connectMongo(); // ⏳ Wait until connected
 
@@ -51,3 +51,7 @@ init();
     app.listen(process.env.PORT, () => {
       console.log(`🚀 Server running on port ${process.env.PORT}`);
     });``
+
+
+
+    export default app;
